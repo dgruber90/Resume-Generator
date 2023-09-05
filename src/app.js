5bd33e53887460d928250ed4d662a5e69b16f12c
Daniel Gruber <144103407+dgruber90@users.noreply.github.com>
2023-09-05 09:57:42 +0200

    Src files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,47 @@
+const { ResumePDF } = require('./generator.js');
 
+const resumeContent = {
+    header: {
+        name: 'Daniel Gruber',
+        phone: '-',
+        email: 'daniel.gruber1990@gmx.at'
+    },
+    summary: {
+        description: 'Aiming to advance my skill to become a full-stack developer.' 
+    },
+    top_skills: {
+        skills: [ 'JavaScript', 'Node.js', 'NPM', 'React.js', 'C#', 'Python'] 
+    },
+    experience: {
+        exp: [
+            {
+                company: "Z-cast",
+                position: "Technical Assistant",
+                year_start: "2020",
+                year_end: "2021",
+                bullets: [
+                    "Assist with online streaming software",
+                ]
+            },
+            {
+                company: "Uanet",
+                position: "Technical Assistant",
+                year_start: "2022",
+                year_end: "Present",
+                bullets: [
+                    "Assist with office management tasks",
+                ]
+            }
+        ]
+    },
+    education: {
+        school: "Akademisches Gymnasium Salzburg",
+        degree: "High School Degree",
+        gpa: "2.5 GPA",
+        grad_year: "2022"
+    }
+}
+
+const myResume = ResumePDF(resumeContent)
+
+myResume.save('MyResume.pdf')
